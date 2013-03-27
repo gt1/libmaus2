5880b40ffef6f7b728f1f26d1caee388b31123bf
German Tischler <gt1@sanger.ac.uk>
2013-03-27 18:39:19 +0000

    add missing file

---
+++ src/libmaus/huffman/KvInitResult.cpp
@@ -0,0 +1,17 @@
+#include <libmaus/huffman/KvInitResult.hpp>
+
+::libmaus::huffman::KvInitResult::KvInitResult()
+: koffset(0), voffset(0), kvoffset(0), kvtarget(0)
+{
+}
+
+std::ostream & operator<<(std::ostream & out, ::libmaus::huffman::KvInitResult const & KIR)
+{
+	out << "KvInitResult("
+		<< "koffset=" << KIR.koffset
+		<< ",voffset=" << KIR.voffset
+		<< ",kvoffset=" << KIR.kvoffset
+		<< ",kvtarget=" << KIR.kvtarget
+		<< ")";
+	return out;
+}
