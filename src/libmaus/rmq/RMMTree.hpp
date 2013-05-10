124a68e144f32667d05568ae1ba354eb167ce05a
German Tischler <gt1@sanger.ac.uk>
2013-05-10 12:04:41 +0100

    add testing for rmm tree

--- src/libmaus/rmq/RMMTree.hpp
+++ src/libmaus/rmq/RMMTree.hpp
@@ -25,7 +25,7 @@ namespace libmaus
 {
 	namespace rmq
 	{
-		template<typename _base_layer_type, unsigned int _klog>
+		template<typename _base_layer_type, unsigned int _klog, bool _rmmtreedebug = false>
 		struct RMMTree
 		{
 			typedef _base_layer_type base_layer_type;
@@ -33,6 +33,7 @@ namespace libmaus
 			static unsigned int const klog = _klog;
 			static unsigned int const k = (1u << klog);
 			static uint64_t const kmask = (k-1);
+			static bool const rmmtreedebug = _rmmtreedebug;
 			
 			base_layer_type const & B;
 			uint64_t const n;
@@ -207,32 +208,31 @@ namespace libmaus
 				for ( uint64_t i = 0; i < numlevels; ++i )
 					S[i+1] = I[i]->size();
 				
-				#if defined(RMMTREEDEBUG)
-				for ( uint64_t kk = k, level = 0; kk < n; kk *= k, ++level )
-				{
-					uint64_t low = 0;
-					uint64_t z = 0;
-					while ( low < n )
+				if ( rmmtreedebug )
+					for ( uint64_t kk = k, level = 0; kk < n; kk *= k, ++level )
 					{
-						uint64_t const high = std::min(low+kk,n);
-						
-						uint64_t minv = B[low];
-						uint64_t mini = low;
-						for ( uint64_t i = low+1; i < high; ++i )
-							if ( B[i] < minv )
-							{
-								minv = B[i];
-								mini = i;
-							}
+						uint64_t low = 0;
+						uint64_t z = 0;
+						while ( low < n )
+						{
+							uint64_t const high = std::min(low+kk,n);
 							
-						assert ( (*C[level])[z] == minv );
-						assert ( (*I[level])[z] == ((mini-low)*k)/kk );
-					
-						++z;	
-						low = high;
+							uint64_t minv = B[low];
+							uint64_t mini = low;
+							for ( uint64_t i = low+1; i < high; ++i )
+								if ( B[i] < minv )
+								{
+									minv = B[i];
+									mini = i;
+								}
+								
+							assert ( (*C[level])[z] == minv );
+							assert ( (*I[level])[z] == ((mini-low)*k)/kk );
+						
+							++z;	
+							low = high;
+						}
 					}
-				}
-				#endif
 			}
 			
 			/*
@@ -282,15 +282,16 @@ namespace libmaus
 							++ni;
 					}
 					
-				#if defined(RMMTREEDEBUG)
-				uint64_t dni = n;
-				for ( uint64_t z = i+1; (dni==n) && z < n; ++z )
-					if ( B[z] < rval )
-						dni = z;
+				if ( rmmtreedebug )
+				{
+					uint64_t dni = n;
+					for ( uint64_t z = i+1; (dni==n) && z < n; ++z )
+						if ( B[z] < rval )
+							dni = z;
 				
-				if ( dni != ni )
-					std::cerr << "i=" << i << " dni=" << dni << " ni=" << ni << " rval=" << rval << std::endl;
-				#endif
+					if ( dni != ni )
+						std::cerr << "i=" << i << " dni=" << dni << " ni=" << ni << " rval=" << rval << std::endl;
+				}
 				
 				return ni;
 			}
@@ -342,17 +343,16 @@ namespace libmaus
 							--nj;
 					}
 				
-				// #define RMMTREEDEBUG	
-				#if defined(RMMTREEDEBUG)
-				int64_t dnj = -1;
-				for ( int64_t z = static_cast<int64_t>(j)-1; (dnj==-1) && z >= 0; --z )
-					if ( B[z] < rval )
-						dnj = z;
+				if ( rmmtreedebug )
+				{
+					int64_t dnj = -1;
+					for ( int64_t z = static_cast<int64_t>(j)-1; (dnj==-1) && z >= 0; --z )
+						if ( B[z] < rval )
+							dnj = z;
 				
-				if ( dnj != nj )
-					std::cerr << "j=" << j << " dnj=" << dnj << " nj=" << nj << " rval=" << rval << std::endl;
-					
-				#endif
+					if ( dnj != nj )
+						std::cerr << "j=" << j << " dnj=" << dnj << " nj=" << nj << " rval=" << rval << std::endl;
+				}			
 				
 				return nj;
 			}
@@ -447,34 +447,33 @@ namespace libmaus
 				
 				// #define RMMTREEDEBUG
 				
-				#if defined(RMMTREEDEBUG)
-				assert ( (*this)(minlevel,mini) == minv );
-				#endif
+				if ( rmmtreedebug )
+					assert ( (*this)(minlevel,mini) == minv );
 
 				// follow path to the position of the minimum
 				while ( minlevel != 0 )
 				{
 					mini = (mini << klog) + I[--minlevel]->get(mini);
 						
-					#if defined(RMMTREEDEBUG)
-					assert ( (*this)(minlevel,mini) == minv );
-					#endif
+					if ( rmmtreedebug )
+						assert ( (*this)(minlevel,mini) == minv );
 				}
 				
-				#if defined(RMMTREEDEBUG)
-				uint64_t dminv = std::numeric_limits<uint64_t>::max();
-				uint64_t dmini = std::numeric_limits<uint64_t>::max();
-				for ( uint64_t z = i; z <= j; ++ z )
-					if ( (*this)(0,z) < dminv )
-					{
-						dminv = (*this)(0,z);
-						dmini = z;
-					}
+				if ( rmmtreedebug )
+				{
+					uint64_t dminv = std::numeric_limits<uint64_t>::max();
+					uint64_t dmini = std::numeric_limits<uint64_t>::max();
+					for ( uint64_t z = i; z <= j; ++ z )
+						if ( (*this)(0,z) < dminv )
+						{
+							dminv = (*this)(0,z);
+							dmini = z;
+						}
 					
-				assert ( minv == dminv );
-				assert ( mini == dmini );
-				#endif
-				
+					assert ( minv == dminv );
+					assert ( mini == dmini );
+				}
+								
 				return mini;
 			}
 		};
