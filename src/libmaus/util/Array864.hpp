730dc1be01bc973f4fcf1911417aa8978ba737a2
German Tischler <gt1@sanger.ac.uk>
2013-05-15 16:27:48 +0100

    add generator class for Array864

--- src/libmaus/util/Array864.hpp
+++ src/libmaus/util/Array864.hpp
@@ -22,11 +22,15 @@
 
 #include <libmaus/rank/ERank222B.hpp>
 #include <libmaus/bitio/getBit.hpp>
+#include <libmaus/util/Histogram.hpp>
+#include <libmaus/util/iterator.hpp>
 
 namespace libmaus
 {
 	namespace util
 	{
+		struct Array864Generator;
+		
 		struct Array864
 		{
 			typedef Array864 this_type;
@@ -36,15 +40,52 @@ namespace libmaus
 			typedef rank_type::writer_type writer_type;
 			typedef writer_type::data_type data_type;
 
+			typedef Array864Generator generator_type;
+			
+			typedef libmaus::util::ConstIterator<this_type,uint64_t> const_iterator;
+
 			uint64_t n;
 			::libmaus::autoarray::AutoArray<data_type> B;
 			::libmaus::rank::ERank222B::unique_ptr_type R;
 			::libmaus::autoarray::AutoArray<uint8_t> A8;
 			::libmaus::autoarray::AutoArray<uint64_t> A64;
 			
+			uint64_t byteSize() const
+			{
+				return
+					sizeof(uint64_t)+
+					B.byteSize()+
+					R->byteSize()+
+					A8.byteSize()+
+					A64.byteSize();
+			}
+			
+			const_iterator begin() const
+			{
+				return const_iterator(this,0);
+			}
+			const_iterator end() const
+			{
+				return const_iterator(this,n);
+			}
+			
 			void serialise(std::ostream & out) const;
 			static uint64_t deserializeNumber(std::istream & in);
+			Array864() : n(0) {}
 			Array864(std::istream & in);
+			
+			static unique_ptr_type load(std::string const & fs)
+			{
+				libmaus::aio::CheckedInputStream CIS(fs);
+				unique_ptr_type u(new this_type(CIS));
+				return UNIQUE_PTR_MOVE(u);
+			}
+
+			static unique_ptr_type load(libmaus::aio::CheckedInputStream & CIS)
+			{
+				unique_ptr_type u(new this_type(CIS));
+				return UNIQUE_PTR_MOVE(u);
+			}
 			
 			template<typename iterator>
 			Array864(iterator a, iterator e)
@@ -107,6 +148,73 @@ namespace libmaus
 					return A8[R->rank1(i)-1];
 				else
 					return A64[R->rank0(i)-1];
+			}
+			
+			uint64_t get(uint64_t const i) const
+			{
+				return (*this)[i];
+			}
+		};
+		
+		struct Array864Generator
+		{
+			uint64_t const n8;
+			uint64_t const n64;
+			uint64_t const n;
+
+			typedef Array864::writer_type writer_type;
+			typedef Array864::data_type data_type;
+			
+			static uint64_t getSmallerEqual(libmaus::util::Histogram & hist, int64_t const thres)
+			{
+				std::map<int64_t,uint64_t> const H = hist.getByType<int64_t>();
+				
+				uint64_t c = 0;
+				
+				for ( std::map<int64_t,uint64_t>::const_iterator ita = H.begin(); ita != H.end(); ++ita )
+					if ( ita->first <= thres )
+						c += ita->second;
+				
+				return c;
+			}
+			
+			Array864::unique_ptr_type P;
+			writer_type::unique_ptr_type W;
+			uint64_t p8;
+			uint64_t p64;
+
+			Array864Generator(libmaus::util::Histogram & hist) 
+			: 
+				n8(getSmallerEqual(hist,8)), n64(getSmallerEqual(hist,64)-n8), n(n8+n64),
+				P(Array864::unique_ptr_type(new Array864)),
+				p8(0), p64(0)
+			{
+				P->B = ::libmaus::autoarray::AutoArray<data_type>((n+63)/64);
+				W = UNIQUE_PTR_MOVE(writer_type::unique_ptr_type(new writer_type(P->B.begin())));
+				P->A8 = ::libmaus::autoarray::AutoArray<uint8_t>(n8);
+				P->A64 = ::libmaus::autoarray::AutoArray<uint64_t>(n64);
+				P->n = n;
+			}
+			
+			void add(uint64_t const v)
+			{
+				if ( v < 256 )
+				{
+					W->writeBit(1);
+					P->A8[p8++] = v;
+				}
+				else
+				{
+					W->writeBit(0);
+					P->A64[p64++] = v;
+				}
+			}
+			
+			Array864::unique_ptr_type createFinal()
+			{
+				W->flush();
+				P->R = UNIQUE_PTR_MOVE(::libmaus::rank::ERank222B::unique_ptr_type(new ::libmaus::rank::ERank222B(P->B.get(), P->B.size()*64)));
+				return UNIQUE_PTR_MOVE(P);
 			}
 		};
 	}
