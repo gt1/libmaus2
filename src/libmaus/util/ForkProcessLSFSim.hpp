3842c20e9bf4d00b6ee73aec1c66959eb8687205
German Tischler <gt1@sanger.ac.uk>
2013-05-30 15:09:20 +0100

    complete docu comments for LSFSim class

--- src/libmaus/util/ForkProcessLSFSim.hpp
+++ src/libmaus/util/ForkProcessLSFSim.hpp
@@ -45,8 +45,22 @@ namespace libmaus
 			 * @param s program name
 			 **/
 			static void initLSF(std::string const & s);
+			/**
+			 * @return true if unique host names can be achieved for multiple jobs
+			 *         (for this class this function always returns false)
+			 **/
 		        static bool distributeUnique();
 		
+		        /**
+		         * constructor
+		         *
+		         * @param scommand command name
+		         * @param maxmem maximum amount of memory in MB
+		         * @param sinfilename input file name, default /dev/null
+		         * @param soutputfilename output file name, default /dev/null
+		         * @param serrfilename error file name, default /dev/null
+		         * @param cwd current working directory for new job
+		         **/
 			ForkProcessLSFSim(
 				std::string const & scommand,
 				std::string const & /* sjobname */,
@@ -61,15 +75,52 @@ namespace libmaus
 				char const * cwd = 0,
 				uint64_t const = 0 /* tmpspace */
 				);
+			/**
+			 * @return process state
+			 **/
 			state getState();
+			/**
+			 * @return true iff process finished
+			 **/
 			bool isFinished();
+			/**
+			 * @return true iff process finished ok
+			 **/
 			bool finishedOk();
+			/**
+			 * wait for process to finish
+			 *
+			 * @param sleepinterval number of seconds to sleep between checking finished()
+			 **/
 			void wait(int sleepinterval = 1);
+			/**
+			 * @return execution host
+			 **/
 			std::string getSingleHost() const;
+			/**
+			 * get list of host names the job is running on 
+			 *
+			 * @param reference to host names vector
+			 * @return true if successful
+			 **/
 			bool getHost(::std::vector<std::string> & hostnames);
+			/**
+			 * @return true if job is known in the scheduling system
+			 **/
 			bool isKnown();
+			/**
+			 * @return true if job is unfinished
+			 **/ 
 			bool isUnfinished();
+			/**
+			 * @return state map for all processes (this map is empty for the LSFSim class)
+			 **/
 			static std::map<int64_t,int64_t> getIntStates();
+			/**
+			 * get state for this job
+			 *
+			 * @return state of this job
+			 **/
 			state getState(std::map<int64_t,int64_t> const &);
 		};
 	}
