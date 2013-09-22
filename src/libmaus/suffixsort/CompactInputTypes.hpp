53af399f6414c1ae89641ca75eb03d9a75afcc92
German Tischler <gt1@sanger.ac.uk>
2013-09-22 20:30:31 +0100

    add input types for suffix sorting/bwt construction

---
+++ src/libmaus/suffixsort/CompactInputTypes.hpp
@@ -0,0 +1,82 @@
+/**
+    libmaus
+    Copyright (C) 2009-2013 German Tischler
+    Copyright (C) 2011-2013 Genome Research Limited
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**/
+#if ! defined(LIBMAUS_SUFFIXSORT_COMPACTINPUTTYPES_HPP)
+#define LIBMAUS_SUFFIXSORT_COMPACTINPUTTYPES_HPP
+
+#include <libmaus/suffixsort/BwtMergeEnumBase.hpp>
+#include <libmaus/bitio/CompactDecoderBuffer.hpp>
+#include <libmaus/aio/CircularWrapper.hpp>
+#include <libmaus/suffixsort/CircularSuffixComparator.hpp>
+#include <libmaus/util/OctetString.hpp>
+
+namespace libmaus
+{
+	namespace suffixsort
+	{
+		struct CompactInputTypes : public libmaus::suffixsort::BwtMergeEnumBase
+		{
+			typedef ::std::istream base_input_stream;
+			typedef ::libmaus::bitio::CompactDecoderWrapper linear_wrapper;
+			typedef ::libmaus::aio::CompactCircularWrapper circular_wrapper;
+			typedef ::libmaus::aio::CompactCircularReverseWrapper circular_reverse_wrapper;
+			typedef ::libmaus::suffixsort::CompactCircularSuffixComparator circular_suffix_comparator;
+			typedef ::libmaus::util::OctetString string_type;
+			typedef ::libmaus::aio::CircularWrapper octet_circular_wrapper;
+			
+			static bwt_merge_sort_input_type getType()
+			{
+				return bwt_merge_input_type_compact;
+			}
+			static bool utf8Wavelet()
+			{
+				return false;
+			}
+
+			static uint64_t getSourceLengthBits(
+				std::string const & fn,
+				uint64_t const low,
+				uint64_t const high,
+				std::map<int64_t,uint64_t> const & /* freqs */)
+			{
+				linear_wrapper L(fn);
+				uint64_t const b = L.getB();
+				return b * (high-low);
+			}
+
+			static uint64_t getSourceLengthBytes(
+				std::string const & /* fn */,
+				uint64_t const low,
+				uint64_t const high,
+				std::map<int64_t,uint64_t> const & /* freqs */)
+			{
+				return (high-low);
+			}
+
+			static uint64_t getSourceTextIndexBits(
+				std::string const & /* fn */,
+				uint64_t const /* low */,
+				uint64_t const /* high */,
+				std::map<int64_t,uint64_t> const & /* freqs */)
+			{
+				return 0;
+			}
+		};
+	}
+}
+#endif
---
+++ src/libmaus/suffixsort/Utf8InputTypes.hpp
@@ -0,0 +1,102 @@
+/**
+    libmaus
+    Copyright (C) 2009-2013 German Tischler
+    Copyright (C) 2011-2013 Genome Research Limited
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**/
+#if ! defined(LIBMAUS_SUFFIXSORT_UTF8INPUTTYPES_HPP)
+#define LIBMAUS_SUFFIXSORT_UTF8INPUTTYPES_HPP
+
+#include <libmaus/suffixsort/BwtMergeEnumBase.hpp>
+#include <libmaus/aio/CircularWrapper.hpp>
+#include <libmaus/suffixsort/CircularSuffixComparator.hpp>
+#include <libmaus/util/Utf8String.hpp>
+
+namespace libmaus
+{
+	namespace suffixsort
+	{
+		struct Utf8InputTypes : public libmaus::suffixsort::BwtMergeEnumBase
+		{
+			typedef ::std::wistream base_input_stream;
+			typedef ::libmaus::util::Utf8DecoderWrapper linear_wrapper;
+			typedef ::libmaus::aio::Utf8CircularWrapperWrapper circular_wrapper;
+			typedef ::libmaus::aio::Utf8CircularReverseWrapperWrapper circular_reverse_wrapper;
+			typedef ::libmaus::suffixsort::Utf8CircularSuffixComparator circular_suffix_comparator;
+			typedef ::libmaus::util::Utf8String string_type;
+			typedef ::libmaus::aio::CircularWrapper octet_circular_wrapper;
+			
+			static bwt_merge_sort_input_type getType()
+			{
+				return bwt_merge_input_type_utf8;
+			}
+			static bool utf8Wavelet()
+			{
+				return true;
+			}
+
+			static uint64_t getSourceLengthBits(
+				std::string const & /* fn */,
+				uint64_t const /* low */,
+				uint64_t const /* high */,
+				std::map<int64_t,uint64_t> const & freqs)
+			{
+				uint64_t sourcelen = 0;
+				
+				for ( std::map<int64_t,uint64_t>::const_iterator ita = freqs.begin();
+					ita != freqs.end(); ++ita )
+				{
+					libmaus::util::CountPutObject C;
+					libmaus::util::UTF8::encodeUTF8(ita->first,C);
+					sourcelen += 8 * C.c * ita->second;
+				}
+				
+				return sourcelen;
+			}
+
+			static uint64_t getSourceLengthBytes(
+				std::string const & /* fn */,
+				uint64_t const /* low */,
+				uint64_t const /* high */,
+				std::map<int64_t,uint64_t> const & freqs)
+			{
+				uint64_t sourcelen = 0;
+				
+				for ( std::map<int64_t,uint64_t>::const_iterator ita = freqs.begin();
+					ita != freqs.end(); ++ita )
+				{
+					libmaus::util::CountPutObject C;
+					libmaus::util::UTF8::encodeUTF8(ita->first,C);
+					sourcelen += C.c * ita->second;
+				}
+				
+				return sourcelen;
+			}
+
+			static uint64_t getSourceTextIndexBits(
+				std::string const & fn,
+				uint64_t const low,
+				uint64_t const high,
+				std::map<int64_t,uint64_t> const & freqs)
+			{
+				uint64_t const vecbits = getSourceLengthBytes(fn,low,high,freqs);
+				uint64_t const rankbits = (vecbits*4+2)/3;
+				uint64_t const selectbits = (vecbits + 255)/256;
+				return vecbits + rankbits + selectbits;
+			}	
+		};
+	}
+}
+#endif
