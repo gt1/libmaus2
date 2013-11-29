5c3c8659ba068ded7e28eae5ce358f187e1bf663
German Tischler <gt1@sanger.ac.uk>
2013-11-29 13:53:32 +0000

    remove const qualifier on static method

--- src/libmaus/bambam/BamAlignmentDecoderBase.hpp
+++ src/libmaus/bambam/BamAlignmentDecoderBase.hpp
@@ -2321,7 +2321,7 @@ namespace libmaus
 			 * @param blocksize size of alignment block
 			 * @return contents of the RG aux field (null pointer if not present)
 			 **/
-			static char const * getReadGroup(uint8_t const * E, uint64_t const blocksize) const
+			static char const * getReadGroup(uint8_t const * E, uint64_t const blocksize)
 			{
 				return getAuxString(E,blocksize,"RG");
 			}
