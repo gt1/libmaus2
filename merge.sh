2929a1510c6d8522de5de8e5ed368e010c509f4d
German Tischler <gt1@sanger.ac.uk>
2013-12-12 22:32:24 +0000

    extend merging script

--- merge.sh
+++ merge.sh
@@ -3,3 +3,10 @@ git checkout master
 git merge experimental
 git push
 git checkout experimental
+
+pushd ../libmaus-debian
+git checkout master
+git merge experimental
+git push
+git checkout experimental
+popd
