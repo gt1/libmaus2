7134fa9d3826752f55cb622f24b521efce107e4e
German Tischler <gt1@sanger.ac.uk>
2014-01-13 12:49:43 +0000

    update merge script

--- merge.sh
+++ merge.sh
@@ -1,10 +1,20 @@
 #! /bin/bash
 git checkout master
+git pull
+git checkout experimental
+git pull
+
+git checkout master
 git merge experimental
 git push
 git checkout experimental
 
 pushd ../libmaus-debian
+git checkout master
+git pull
+git checkout experimental
+git pull
+
 git checkout master
 git merge experimental
 git push
