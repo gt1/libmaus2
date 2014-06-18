07d185048570c67fdb27784daa98085af8acc40d
German Tischler <gt1@sanger.ac.uk>
2014-06-18 10:04:09 +0100

    add Darwin support in build script for igzip

--- contrib/build_igzip.sh
+++ contrib/build_igzip.sh
@@ -1,7 +1,22 @@
 #! /bin/bash
+function download
+{
+	CURL=`which curl`
+	WGET=`which wget`
+	
+	if [ -e ${CURL} ] ; then
+		curl ${1} > `basename ${1}`
+	elif [ -e ${WGET} ] ; then
+		wget ${1}
+	else
+		echo "No download program found"
+		exit 1
+	fi
+}
+
 if [ ! -f igzip-src.tar.gz ] ; then
 	if [ ! -f igzip_042.zip ] ; then
-		wget https://software.intel.com/sites/default/files/managed/2d/63/igzip_042.zip
+		download https://software.intel.com/sites/default/files/managed/2d/63/igzip_042.zip > igzip_042.zip
 	fi
 
 	mkdir -p igzip-src
@@ -16,7 +31,7 @@ fi
 YASMVERSION=1.2.0
 
 if [ ! -f yasm-${YASMVERSION}.tar.gz ] ; then
-	wget http://www.tortall.net/projects/yasm/releases/yasm-${YASMVERSION}.tar.gz
+	download http://www.tortall.net/projects/yasm/releases/yasm-${YASMVERSION}.tar.gz
 fi
 
 BASEDIR=${PWD}
@@ -37,6 +52,10 @@ sed < Makefile "s|^YASM *:= *.*|YASM := ${BASEDIR}/yasm-bin/${YASMVERSION}/bin/y
 mv Makefile.patched Makefile
 sed < options.inc "s|^;%define GENOME_SAM|%define GENOME_SAM|;s|^;%define GENOME_BAM|%define GENOME_BAM|;s|;%define ONLY_DEFLATE|%define ONLY_DEFLATE|" > options.inc.patched
 mv options.inc.patched options.inc
+if [ "`uname`" = "Darwin" ] ; then
+	sed < Makefile "s|-f elf64|-f macho64|;s|-g dwarf2|-g null --prefix=_|" > Makefile.patched
+	mv Makefile.patched Makefile
+fi
 make
 cd ../..
 
@@ -45,7 +64,7 @@ rm -fR ${BASEDIR}/yasm-bin
 mkdir -p igzip-bin
 mkdir -p igzip-bin/lib
 mkdir -p igzip-bin/include
-for i in `find igzip-src -regextype posix-extended -regex ".*\.(a|so)"` ; do
+for i in igzip-src/igzip/*.so igzip-src/igzip/*.a ; do
 	cp -p $i igzip-bin/lib/
 done
 for i in igzip-src/include/types.h igzip-src/igzip/c_code/igzip_lib.h igzip-src/igzip/c_code/internal_state_size.h ; do
