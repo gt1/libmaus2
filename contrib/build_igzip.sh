8f9de79e961d75094095b60dc686dfaff91c52e4
German Tischler <gt1@sanger.ac.uk>
2014-06-18 14:57:33 +0100

    - add option to install library in given place - improvements for installation on Darwin

--- contrib/build_igzip.sh
+++ contrib/build_igzip.sh
@@ -16,7 +16,7 @@ function download
 
 if [ ! -f igzip-src.tar.gz ] ; then
 	if [ ! -f igzip_042.zip ] ; then
-		download https://software.intel.com/sites/default/files/managed/2d/63/igzip_042.zip
+		download https://software.intel.com/sites/default/files/managed/2d/63/igzip_042.zip > igzip_042.zip
 	fi
 
 	mkdir -p igzip-src
@@ -53,18 +53,21 @@ mv Makefile.patched Makefile
 sed < options.inc "s|^;%define GENOME_SAM|%define GENOME_SAM|;s|^;%define GENOME_BAM|%define GENOME_BAM|;s|;%define ONLY_DEFLATE|%define ONLY_DEFLATE|" > options.inc.patched
 mv options.inc.patched options.inc
 if [ "`uname`" = "Darwin" ] ; then
-	sed < Makefile "s|-f elf64|-f macho64|;s|-g dwarf2|-g null --prefix=_|" > Makefile.patched
+	sed < Makefile "s|-f elf64|-f macho64|;s|-g dwarf2|-g null --prefix=_|;s|-shared|-dynamiclib -current_version 1.0 -compatibility_version 1.0 -fvisibility=hidden -Wl,-install_name,\$@|;s|libigzip0.so|libigzip0.dylib|;s|libigzip0c.so|libigzip0c.dylib|;s|libigzip1.so|libigzip1.dylib|;s|libigzip1c.so|libigzip1c.dylib|" > Makefile.patched
 	mv Makefile.patched Makefile
 fi
 make
 cd ../..
 
-rm -fR ${BASEDIR}/yasm-bin
+SOSUFFIX=.so
+if [ "`uname`" = "Darwin" ] ; then
+	SOSUFFIX=.dylib
+fi
 
 mkdir -p igzip-bin
 mkdir -p igzip-bin/lib
 mkdir -p igzip-bin/include
-for i in igzip-src/igzip/*.so igzip-src/igzip/*.a ; do
+for i in igzip-src/igzip/*${SOSUFFIX} igzip-src/igzip/*.a ; do
 	cp -p $i igzip-bin/lib/
 done
 for i in igzip-src/include/types.h igzip-src/igzip/c_code/igzip_lib.h igzip-src/igzip/c_code/internal_state_size.h ; do
@@ -81,6 +84,25 @@ tar czf igzip-bin.tar.gz igzip-bin
 
 rm -fR igzip-src
 rm -fR igzip-bin
+rm -fR ${BASEDIR}/yasm-bin
 
 rm -fR yasm-${YASMVERSION}.tar.gz
 rm -fR igzip-src.tar.gz
+
+if [ $# -ge 1 ] ; then
+	tar xzvf igzip-bin.tar.gz
+	cd igzip-bin
+	INSTDIR=$1
+	mkdir -p ${INSTDIR}
+	tar cf - . | tar xf - -C ${INSTDIR}
+	
+	if [ "`uname`" = "Darwin" ] ; then
+		SDIR=${PWD}
+		cd ${INSTDIR}/lib
+		for i in *.dylib ; do install_name_tool -change $i ${PWD}/$i -id ${PWD}/${i} ${i} ; done
+		cd ${SDIR}
+	fi
+	
+	cd ..
+	rm -fR igzip-bin
+fi
