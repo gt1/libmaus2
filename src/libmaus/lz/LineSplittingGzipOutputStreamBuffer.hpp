266c316e9f155d4db925ae000012eda3990df375
German Tischler <gt1@sanger.ac.uk>
2014-09-16 23:43:27 +0100

    avoid flushing zlib state when no new data was written

--- src/libmaus/lz/LineSplittingGzipOutputStreamBuffer.hpp
+++ src/libmaus/lz/LineSplittingGzipOutputStreamBuffer.hpp
@@ -49,13 +49,40 @@ namespace libmaus
 			uint64_t usize;
 			
 			bool reopenpending;
+			bool terminated;
+			bool flushed;
 			
 			std::string openfilename;
+			
+			static int doDeflate(z_stream * strm, int flush)
+			{
+				#if defined(LINESPLITTINGGZIPOUTPUTSTREAMDEBUG)
+				std::cerr << "deflate(";
+				
+				switch ( flush )
+				{
+					case Z_NO_FLUSH: std::cerr << "Z_NO_FLUSH"; break;
+					case Z_PARTIAL_FLUSH: std::cerr << "Z_PARTIAL_FLUSH"; break;
+					case Z_SYNC_FLUSH: std::cerr << "Z_SYNC_FLUSH"; break;
+					case Z_FULL_FLUSH: std::cerr << "Z_FULL_FLUSH"; break;
+					case Z_FINISH: std::cerr << "Z_FINISH"; break;				
+				}
+				
+				std::cerr << ")\n";
+				#endif
+			
+				return ::deflate(strm,flush);
+			}
 
 			void doInit(int const level)
 			{
+				#if defined(LINESPLITTINGGZIPOUTPUTSTREAMDEBUG)
+				std::cerr << "doInit()" << std::endl;
+				#endif
+			
 				isize = 0;
 				usize = 0;
+				flushed = true;
 				crc = crc32(0,0,0);
 				
 				memset ( &strm , 0, sizeof(z_stream) );
@@ -66,8 +93,12 @@ namespace libmaus
 					8 /* mem level, gzip default */, Z_DEFAULT_STRATEGY);
 				if ( ret != Z_OK )
 				{
+					#if defined(LINESPLITTINGGZIPOUTPUTSTREAMDEBUG)
+					std::cerr << "doInit() deflateInit2 failed" << std::endl;
+					#endif
+				
 					::libmaus::exception::LibMausException se;
-					se.getStream() << "LineSplittingGzipOutputStreamBuffer::inti(): deflateInit2 failed";
+					se.getStream() << "LineSplittingGzipOutputStreamBuffer::doInit(): deflateInit2 failed";
 					se.finish();
 					throw se;
 				}
@@ -86,14 +117,19 @@ namespace libmaus
 				{
 					strm.avail_out = outbuffer.size();
 					strm.next_out  = reinterpret_cast<Bytef *>(outbuffer.begin());
-					int ret = deflate(&strm,Z_NO_FLUSH);
-					if ( ret == Z_STREAM_ERROR )
+					int ret = doDeflate(&strm,Z_NO_FLUSH);
+					if ( ret < 0 )
 					{
+						#if defined(LINESPLITTINGGZIPOUTPUTSTREAMDEBUG)
+						std::cerr << "doSync() deflate failed" << std::endl;
+						#endif
+					
 						::libmaus::exception::LibMausException se;
-						se.getStream() << "deflate failed (Z_STREAM_ERROR).";
+						se.getStream() << "LineSplittingGzipOutputStreamBuffer::doSync: deflate failed: " << ret << " (" << strm.msg << ")";
 						se.finish();
 						throw se;
 					}
+					flushed = false;
 					uint64_t const have = outbuffer.size() - strm.avail_out;
 					Pout->write(reinterpret_cast<char const *>(outbuffer.begin()),have);
 				} 
@@ -139,28 +175,36 @@ namespace libmaus
 
 			void doFullFlush()
 			{
-				int ret;
-				
-				do
+				if ( (! terminated) && (!flushed) )
 				{
-					strm.avail_in = 0;
-					strm.next_in = 0;
-					strm.avail_out = outbuffer.size();
-					strm.next_out = reinterpret_cast<Bytef *>(outbuffer.begin());
-					ret = deflate(&strm,Z_FULL_FLUSH);
-					if ( ret == Z_STREAM_ERROR )
-					{
-						::libmaus::exception::LibMausException se;
-						se.getStream() << "LineSplittingGzipOutputStreamBuffer::deflate() failed.";
-						se.finish();
-						throw se;
-					}
-					uint64_t have = outbuffer.size() - strm.avail_out;
-					Pout->write(reinterpret_cast<char const *>(outbuffer.begin()),have);
+					int ret;
 					
-				} while (strm.avail_out == 0);
+					do
+					{
+						strm.avail_in = 0;
+						strm.next_in = 0;
+						strm.avail_out = outbuffer.size();
+						strm.next_out = reinterpret_cast<Bytef *>(outbuffer.begin());
+						ret = doDeflate(&strm,Z_FULL_FLUSH);
+						if ( ret < 0 )
+						{
+							#if defined(LINESPLITTINGGZIPOUTPUTSTREAMDEBUG)
+							std::cerr << "doFullFlush deflate failed" << std::endl;
+							#endif
 						
-				assert ( ret == Z_OK );
+							::libmaus::exception::LibMausException se;
+							se.getStream() << "LineSplittingGzipOutputStreamBuffer::doFullFlush: deflate failed with error " << ret << " (" << strm.msg << ")";
+							se.finish();
+							throw se;
+						}
+						flushed = true;
+						uint64_t have = outbuffer.size() - strm.avail_out;
+						Pout->write(reinterpret_cast<char const *>(outbuffer.begin()),have);
+						
+					} while (strm.avail_out == 0);
+											
+					assert ( ret == Z_OK );
+				}
 			}
 
 			void doFinish()
@@ -173,14 +217,19 @@ namespace libmaus
 					strm.next_in = 0;
 					strm.avail_out = outbuffer.size();
 					strm.next_out = reinterpret_cast<Bytef *>(outbuffer.begin());
-					ret = deflate(&strm,Z_FINISH );
+					ret = doDeflate(&strm,Z_FINISH);
 					if ( ret == Z_STREAM_ERROR )
 					{
+						#if defined(LINESPLITTINGGZIPOUTPUTSTREAMDEBUG)
+						std::cerr << "doFinish deflate failed" << std::endl;
+						#endif
+					
 						::libmaus::exception::LibMausException se;
-						se.getStream() << "LineSplittingGzipOutputStreamBuffer::deflate() failed.";
+						se.getStream() << "LineSplittingGzipOutputStreamBuffer::doFinish(): deflate failed: " << ret << " (" << strm.msg << ")";
 						se.finish();
 						throw se;
 					}
+					flushed = true;
 					uint64_t have = outbuffer.size() - strm.avail_out;
 					Pout->write(reinterpret_cast<char const *>(outbuffer.begin()),have);
 					
@@ -211,6 +260,11 @@ namespace libmaus
 				
 				openfilename = std::string();
 				usize = 0;
+				terminated = true;
+
+				#if defined(LINESPLITTINGGZIPOUTPUTSTREAMDEBUG)
+				std::cerr << "doTerminate() " << terminated << std::endl;
+				#endif
 			}
 			
 			void doOpen()
@@ -230,6 +284,7 @@ namespace libmaus
 				doInit(level);
 				
 				openfilename = filename;
+				terminated = false;
 			}
 
 			public:
@@ -239,7 +294,7 @@ namespace libmaus
 				uint64_t const rbuffersize, 
 				int const rlevel = Z_DEFAULT_COMPRESSION
 			)
-			: fn(rfn), level(rlevel), fileno(0), linemod(rlinemod), linecnt(0), buffersize(rbuffersize), inbuffer(buffersize,false), outbuffer(buffersize,false), reopenpending(false)
+			: fn(rfn), level(rlevel), fileno(0), linemod(rlinemod), linecnt(0), buffersize(rbuffersize), inbuffer(buffersize,false), outbuffer(buffersize,false), reopenpending(false), terminated(true), flushed(false)
 			{
 				doOpen();
 				setp(inbuffer.begin(),inbuffer.end()-1);
@@ -247,19 +302,35 @@ namespace libmaus
 
 			~LineSplittingGzipOutputStreamBuffer()
 			{
+				#if defined(LINESPLITTINGGZIPOUTPUTSTREAMDEBUG)
+				std::cerr << "Destructor doSync()" << std::endl;
+				#endif
+			
 				doSync();
 				
 				bool const deletefile = (fileno==1) && (usize==0);
 				std::string deletefilename = openfilename;
 				
+				#if defined(LINESPLITTINGGZIPOUTPUTSTREAMDEBUG)
+				std::cerr << "Destructor doTerminate()" << std::endl;
+				#endif
+				
 				doTerminate();
 				
 				if ( deletefile )
 					remove(deletefilename.c_str());
+					
+				#if defined(LINESPLITTINGGZIPOUTPUTSTREAMDEBUG)
+				std::cerr << "Destructor done." << std::endl;
+				#endif
 			}
 
 			int_type overflow(int_type c = traits_type::eof())
 			{
+				#if defined(LINESPLITTINGGZIPOUTPUTSTREAMDEBUG)
+				std::cerr << "overflow()" << std::endl;
+				#endif
+				
 				if ( c != traits_type::eof() )
 				{
 					*pptr() = c;
@@ -272,12 +343,38 @@ namespace libmaus
 	
 			int sync()
 			{
+				#if defined(LINESPLITTINGGZIPOUTPUTSTREAMDEBUG)
+				std::cerr << "sync()" << std::endl;
+				#endif
+				
 				// flush input buffer
 				doSync();
-				// flush zlib state
-				doFullFlush();
+				
+				#if defined(LINESPLITTINGGZIPOUTPUTSTREAMDEBUG)
+				std::cerr << "sync doSync() done" << std::endl;
+				#endif
+
+				if ( ! terminated )
+				{
+					#if defined(LINESPLITTINGGZIPOUTPUTSTREAMDEBUG)
+					std::cerr << "sync() entring doFullFlush" << std::endl;
+					#endif
+				
+					// flush zlib state
+					doFullFlush();
+				}
+			
+				#if defined(LINESPLITTINGGZIPOUTPUTSTREAMDEBUG)
+				std::cerr << "sync() flushing output stream" << std::endl;
+				#endif
+					
 				// flush output stream
 				Pout->flush();	
+			
+				#if defined(LINESPLITTINGGZIPOUTPUTSTREAMDEBUG)
+				std::cerr << "sync() done." << std::endl;
+				#endif
+			
 				return 0; // no error, -1 for error
 			}			
 		};
