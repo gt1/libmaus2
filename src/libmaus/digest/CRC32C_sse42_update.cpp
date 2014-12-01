8b02ce99cd4126cf0e90b731396b78625330b84a
German Tischler <gt1@sanger.ac.uk>
2014-12-01 09:54:35 +0000

    fix input alignment in SSE4.2 based CRC32C

--- src/libmaus/digest/CRC32C_sse42_update.cpp
+++ src/libmaus/digest/CRC32C_sse42_update.cpp
@@ -31,27 +31,25 @@ void libmaus::digest::CRC32C_sse42::update(uint8_t const * t, size_t l)
 		
 	size_t const offset = reinterpret_cast<size_t>(t);
 		
+	// check for 3 LSB
 	if ( offset & 7 )
 	{
-		if ( offset & 3 )
+		// check for LSB
+		if ( (offset & 1) && l )
 		{
-			if ( offset & 1 )
-			{
-				if ( l )
-				{
-					ctx = _mm_crc32_u8(ctx, *t);
-					t += 1;
-					l -= 1;
-				}
-			}
-			if ( l >= 2 )
-			{
-				ctx = _mm_crc32_u16(ctx, *reinterpret_cast<uint16_t const *>(t));
-				t += 2;
-				l -= 2;			
-			}
+			ctx = _mm_crc32_u8(ctx, *t);
+			t += 1;
+			l -= 1;
+		}
+		// check for 2nd LSB
+		if ( (offset & 2) && (l>=2) )
+		{
+			ctx = _mm_crc32_u16(ctx, *reinterpret_cast<uint16_t const *>(t));
+			t += 2;
+			l -= 2;			
 		}	
-		if ( l >= 4 )
+		// check for 3rd LSB
+		if ( (offset & 4) && l >= 4 )
 		{
 			ctx = _mm_crc32_u32(ctx, *reinterpret_cast<uint32_t const *>(t));
 			t += 4;
