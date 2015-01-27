2fc2f3b294de2ebd49a058f7339855c2806de790
German Tischler <gt1@sanger.ac.uk>
2015-01-27 11:33:51 +0000

    fix include guard

--- src/libmaus/irods/IRodsInputStreamBuffer.hpp
+++ src/libmaus/irods/IRodsInputStreamBuffer.hpp
@@ -17,7 +17,7 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #if !defined(LIBMAUS_IRODS_IRODSINPUTSTREAMBUFFER_HPP)
-#define LIBMAUS_IRODS_IRODSINPUTSTREAMBUFFER
+#define LIBMAUS_IRODS_IRODSINPUTSTREAMBUFFER_HPP
 
 #include <streambuf>
 #include <istream>
