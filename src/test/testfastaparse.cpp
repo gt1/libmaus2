06581c3ed5671f005a6492148585d6c9c6ef3b52
German Tischler <gt1@sanger.ac.uk>
2015-03-10 09:36:00 +0000

    add generating of randomly changed sequence in testfastaparse

--- src/test/testfastaparse.cpp
+++ src/test/testfastaparse.cpp
@@ -1,7 +1,7 @@
 /*
     libmaus
-    Copyright (C) 2009-2014 German Tischler
-    Copyright (C) 2011-2014 Genome Research Limited
+    Copyright (C) 2009-2015 German Tischler
+    Copyright (C) 2011-2015 Genome Research Limited
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -24,6 +24,9 @@
 #include <libmaus/timing/RealTimeClock.hpp>
 #include <libmaus/aio/PosixFdInputStream.hpp>
 
+#include <libmaus/wavelet/DynamicWaveletTree.hpp>
+#include <libmaus/random/Random.hpp>
+
 #include <iostream>
 
 int main()
@@ -40,8 +43,13 @@ int main()
 	std::pair<std::string,::libmaus::fastx::FastAStream::shared_ptr_type> P;
 	uint64_t bases = 0;
 	
+	double const erate = 0.03;
+	libmaus::random::Random::setup();
+	
 	while ( FASS.getNextStream(P) )
 	{
+		libmaus::wavelet::DynamicWaveletTree<8,8> D(2);
+	
 		std::string const & id = P.first;
 		::libmaus::fastx::FastAStream & basestream = *(P.second);
 		
@@ -50,13 +58,85 @@ int main()
 		{
 			basestream.read(B.begin(),B.size());
 			uint64_t gcnt = basestream.gcount();
+			
+			for ( size_t i = 0; i < gcnt; ++i )
+			{
+				char const c = B[i];
+				
+				if ( c == 'A' )
+					D.insert(0,cnt+i);
+				else if ( c == 'C' )
+					D.insert(1,cnt+i);
+				else if ( c == 'G' )
+					D.insert(2,cnt+i);
+				else if ( c == 'T' )
+					D.insert(3,cnt+i);
+				else
+					D.insert(libmaus::random::Random::rand8()%4,cnt+i);
+			}
+			
 			cnt += gcnt;
 			
+			std::cerr << "\r" << std::string(80,' ') << "\r" << cnt;
+						
 			if ( !gcnt )
 				break;
 		}
+		
+		std::cerr << "\r" << std::string(80,' ') << "\r" << cnt << "\n";
 
-		std::cout << id << "\t" << cnt << "\n";
+		uint64_t const errors = (D.size() * erate + 0.5);
+		
+		// std::cerr << "[V] Injecting " << errors << " errors." << std::endl;
+		
+		for ( uint64_t i = 0; i < errors; ++i )
+		{
+			unsigned int const errtype = libmaus::random::Random::rand8() % 3;
+			switch ( errtype )
+			{
+				case 0:
+					{
+						uint64_t const inpos = libmaus::random::Random::rand64() % (D.size()+1);
+						unsigned int const inbase = libmaus::random::Random::rand8() % 4;
+						D.insert(inbase,inpos);
+						break;
+					}
+				case 1:
+					if ( D.size() )
+					{
+						uint64_t const delpos = libmaus::random::Random::rand64() % (D.size());
+						D.remove(delpos);
+					}
+					break;
+				case 2:
+				{
+					if ( D.size() )
+					{
+						uint64_t const subspos = libmaus::random::Random::rand64() % (D.size());
+						unsigned int const inbase = libmaus::random::Random::rand8() % 4;
+						D.remove(subspos);
+						D.insert(inbase,subspos);
+					}
+					break;
+				}
+			}
+		}
+
+		std::cerr << id << "\t" << cnt << "\n";
+		
+		std::cout << ">" << P.first << "\n";
+		for ( uint64_t i = 0; i < D.size(); ++i )
+		{
+			unsigned int const c = D[i];
+			switch ( c )
+			{
+				case 0: std::cout.put('A'); break;
+				case 1: std::cout.put('C'); break;
+				case 2: std::cout.put('G'); break;
+				case 3: std::cout.put('T'); break;
+			}
+		}
+		std::cout.put('\n');
 		
 		bases += cnt;
 	}
