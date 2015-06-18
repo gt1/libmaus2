47424ba9b4c6e0b9f45ead084c9cc7f87ea3d277
German Tischler <tischler@mpi-cbg.de>
2015-06-18 10:26:01 +0200

    add bit vector approximate matching algorithm

---
+++ src/libmaus2/lcs/BitVectorResultCallbackInterface.hpp
@@ -0,0 +1,32 @@
+/*
+    libmaus2
+    Copyright (C) 2015 German Tischler
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#if ! defined(LIBMAUS2_LCS_BITVECTORRESULTCALLBACKINTERFACE_HPP)
+#define LIBMAUS2_LCS_BITVECTORRESULTCALLBACKINTERFACE_HPP
+
+namespace libmaus2
+{
+	namespace lcs
+	{
+		struct BitVectorResultCallbackInterface
+		{
+			~BitVectorResultCallbackInterface() {}
+			virtual void operator()(size_t const p, uint64_t const distance) = 0;
+		};
+	}
+}
+#endif
