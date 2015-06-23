fa3982033f8c19a4b2d49a7cbd93f479b18c57cf
German Tischler <gt1@sanger.ac.uk>
2015-06-23 23:52:59 +0200

    use condition variable instead of semaphore

--- src/libmaus2/parallel/TerminatableSynchronousHeap.hpp
+++ src/libmaus2/parallel/TerminatableSynchronousHeap.hpp
@@ -27,9 +27,8 @@ namespace libmaus2
 {
         namespace parallel
         {
-                #if defined(__APPLE__)
                 /**
-                 * posix condition variable based version for Darwin, which has limited support for posix semaphores (no timed waits)
+                 * posix condition variable based version
                  **/
                 template<typename _value_type, typename _compare = ::std::less<_value_type> >
                 struct TerminatableSynchronousHeap
@@ -218,60 +217,6 @@ namespace libmaus2
 				return V;
                         }
                 };
-                #else
-                template<typename value_type, typename compare = ::std::less<value_type> >
-                struct TerminatableSynchronousHeap : public SynchronousHeap<value_type,compare>
-                {
-                        typedef SynchronousHeap<value_type,compare> parent_type;
-
-                        PosixSpinLock terminatelock;
-                        volatile uint64_t terminated;
-                        uint64_t const terminatedthreshold;
-                        
-                        TerminatableSynchronousHeap(uint64_t const rterminatedthreshold = 1)
-                        : terminated(0), terminatedthreshold(rterminatedthreshold)
-                        {
-                        }
-
-                        TerminatableSynchronousHeap(compare const & comp, uint64_t const rterminatedthreshold = 1)
-                        : parent_type(comp), terminated(0), terminatedthreshold(rterminatedthreshold)
-                        {
-                        }
-                        
-                        void terminate()
-                        {
-                                terminatelock.lock();
-                                terminated++;
-                                terminatelock.unlock();
-                        }
-                        bool isTerminated()
-                        {
-                                terminatelock.lock();
-                                bool const isTerm = terminated >= terminatedthreshold;
-                                terminatelock.unlock();
-                                return isTerm;
-                        }
-                        
-                        value_type deque()
-                        {
-                                while ( !parent_type::semaphore.timedWait() )
-                                {
-                                        terminatelock.lock();
-                                        bool const isterminated = terminated >= terminatedthreshold;
-                                        terminatelock.unlock();
-                                        
-                                        if ( isterminated )
-                                                throw std::runtime_error("Heap is terminated");
-                                }
-                                
-                                parent_type::lock.lock();
-                                value_type const v = parent_type::Q.top();
-                                parent_type::Q.pop();
-                                parent_type::lock.unlock();
-                                return v;
-                        }
-                };
-                #endif
         }
 }
 #endif
