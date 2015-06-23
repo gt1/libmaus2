c4a50a63b6461a1b9c8b4b0216bc7e80589a5032
German Tischler <gt1@sanger.ac.uk>
2015-06-23 23:53:16 +0200

    use condition variable instead of semaphore

--- src/libmaus2/parallel/TerminatableSynchronousQueue.hpp
+++ src/libmaus2/parallel/TerminatableSynchronousQueue.hpp
@@ -27,9 +27,8 @@ namespace libmaus2
 {
         namespace parallel
         {
-                #if defined(__APPLE__)
                 /**
-                 * posix condition variable based version for Darwin, which has limited support for posix semaphores (no timed waits)
+                 * posix condition variable based version
                  **/
                 template<typename _value_type>
                 struct TerminatableSynchronousQueue
@@ -177,54 +176,6 @@ namespace libmaus2
                                 }
                         }
                 };
-                #else
-                template<typename value_type>
-                struct TerminatableSynchronousQueue : public SynchronousQueue<value_type>
-                {
-                        typedef SynchronousQueue<value_type> parent_type;
-
-                        PosixMutex terminatelock;
-                        volatile bool terminated;
-                        
-                        TerminatableSynchronousQueue()
-                        {
-                                terminated = false;
-                        }
-                        
-                        void terminate()
-                        {
-                                terminatelock.lock();
-                                terminated = true;
-                                terminatelock.unlock();
-                        }
-                        bool isTerminated()
-                        {
-                                terminatelock.lock();
-                                bool const isTerm = terminated;
-                                terminatelock.unlock();
-                                return isTerm;
-                        }
-                        
-                        value_type deque()
-                        {
-                                while ( !parent_type::semaphore.timedWait() )
-                                {
-                                        terminatelock.lock();
-                                        bool const isterminated = terminated;
-                                        terminatelock.unlock();
-                                        
-                                        if ( isterminated )
-                                                throw std::runtime_error("Queue is terminated");
-                                }
-                                
-                                parent_type::lock.lock();
-                                value_type const v = parent_type::Q.front();
-                                parent_type::Q.pop_front();
-                                parent_type::lock.unlock();
-                                return v;
-                        }
-                };
-                #endif
         }
 }
 #endif
