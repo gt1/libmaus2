d0cfbfe726f7c61978e7f9787b4ec2ac2dab32cf
German Tischler <tischler@mpi-cbg.de>
2015-09-21 18:18:57 +0200

    - add alternative code in RepetetiveKmerFreqEstimate class based on GMP fixed point numbers - use binary search instead of linear scan to find highest value still holding the threshold

--- src/libmaus2/math/RepetetiveKmerFreqEstimate.hpp
+++ src/libmaus2/math/RepetetiveKmerFreqEstimate.hpp
@@ -31,22 +31,66 @@ namespace libmaus2
 				uint64_t const period,
 				uint64_t const k,
 				double const thres = 0.99,
-				double const e = 0.15
+				double const e = 0.15,
+				unsigned int
+				#if defined(LIBMAUS2_HAVE_GMP)
+					prec = 8192 // fixed point precision
+				#endif
 			)
 			{
+				#if defined(LIBMAUS2_HAVE_GMP)
+				GmpFloat const p(1.0 - 2.0*e,prec); // correct base probability (two reads)
+				GmpFloat const pk = Binom::slowPow(p,k,prec);
+				GmpFloat const gthres(thres,prec);
+				uint64_t const numinst = replen / period;
+
+				// binary search on monotonous function
+				unsigned int low = 0, high = numinst + 1;
+				while ( high - low > 1 )
+				{
+					uint64_t const m = (high+low)/2;
+					GmpFloat const v = Binom::binomRowUpperGmpFloat(pk,m,numinst,prec);
+
+					if ( v < gthres )
+						high = m;
+					else
+						low = m;
+				}
+
+				uint64_t i = low;
+
+				while ( Binom::binomRowUpperGmpFloat(pk,i,numinst,prec) >= gthres )
+					++i;
+
+				if ( i )
+					i -= 1;
+
+				return std::pair<uint64_t,double>(i,static_cast<double>(Binom::binomRowUpperGmpFloat(pk,i,numinst,prec)));
+				#else
 				double const p = 1.0 - 2.0*e; // correct base probability (two reads)
 				double const pk = std::pow(p,k);
 				uint64_t const numinst = replen / period;
 			
-				uint64_t i = 0;
-					
-				while ( libmaus2::math::Binom::binomRowUpper(pk,i,numinst) >= thres )
-					++i;
-						
+				// binary search on monotonous function
+				unsigned int low = 0, high = numinst + 1;
+				while ( high - low > 1 )
+				{
+					uint64_t const m = (high+low)/2;
+					float const v = Binom::binomRowUpper(pk,m,numinst);
+
+					if ( v < thres )
+						high = m;
+					else
+						low = m;
+				}
+
+				uint64_t i = low;
+
 				if ( i )
 					i -= 1;
-					
+
 				return std::pair<uint64_t,double>(i,libmaus2::math::Binom::binomRowUpper(pk,i,numinst));
+				#endif
 			}
 		};
 	}
