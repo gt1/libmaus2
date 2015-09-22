70da7201d8bcbaa77a12485116c1fb52705fd459
German Tischler <tischler@mpi-cbg.de>
2015-09-22 10:50:28 +0200

    add TracePoint class

---
+++ src/libmaus2/dazzler/align/TracePoint.hpp
@@ -0,0 +1,51 @@
+/*
+    libmaus2
+    Copyright (C) 2015 German Tischler
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#if ! defined(LIBMAUS2_DAZZLER_ALIGN_TRACEPOINT_HPP)
+#define LIBMAUS2_DAZZLER_ALIGN_TRACEPOINT_HPP
+
+#include <libmaus2/types/types.hpp>
+#include <ostream>
+
+namespace libmaus2
+{
+	namespace dazzler
+	{
+		namespace align
+		{
+			struct TracePoint
+			{
+				int64_t bpos;
+				int64_t epos;
+				int64_t id;
+				
+				TracePoint() {}
+				TracePoint(
+					int64_t const rbpos,
+					int64_t const repos,
+					int64_t const rid	
+				) : bpos(rbpos), epos(repos),id(rid)
+				{
+				
+				}
+			};
+			
+			std::ostream & operator<<(std::ostream & out, TracePoint const & TP);
+		}
+	}
+}
+#endif
