9f744023587e51a6b99ee858d564bac40c02ae64
German Tischler <tischler@mpi-cbg.de>
2015-10-16 13:10:38 +0200

    add xz/lzma decoding support class

---
+++ src/test/testxz.cpp
@@ -0,0 +1,37 @@
+/*
+    libmaus2
+    Copyright (C) 2015 German Tischler
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include <libmaus2/lz/XzDecoder.hpp>
+#include <iostream>
+
+int main()
+{
+	try
+	{
+		int const bufsize = 8192;
+		char inbuf[8192];
+		libmaus2::lz::XzDecoder xzin(std::cin,1024,1024);
+		size_t r = 0;
+		while ( (r = xzin.read(reinterpret_cast<uint8_t *>(&inbuf[0]),bufsize)) != 0 )
+			std::cout.write(&inbuf[0],r);
+	}
+	catch(std::exception const & ex)
+	{
+		std::cerr << ex.what() << std::endl;
+		return EXIT_FAILURE;
+	}
+}
