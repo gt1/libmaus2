a34dcc118b33aa47a0ad6a7870c3d5fc34d16155
German Tischler <tischler@mpi-cbg.de>
2015-12-24 14:21:16 +0100

    add generic LowBits class (lowbits function is for uint64_t only)

--- src/libmaus2/math/lowbits.hpp
+++ src/libmaus2/math/lowbits.hpp
@@ -1,6 +1,6 @@
 /*
     libmaus2
-    Copyright (C) 2009-2013 German Tischler
+    Copyright (C) 2009-2015 German Tischler
     Copyright (C) 2011-2013 Genome Research Limited
 
     This program is free software: you can redistribute it and/or modify
@@ -17,10 +17,11 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#if ! defined(LOWBITS_HPP)
-#define LOWBITS_HPP
+#if ! defined(LIBMAUS2_MATH_LOWBITS_HPP)
+#define LIBMAUS2_MATH_LOWBITS_HPP
 
 #include <libmaus2/types/types.hpp>
+#include <climits>
 
 namespace libmaus2
 {
@@ -34,6 +35,30 @@ namespace libmaus2
 				return 0xFFFFFFFFFFFFFFFFULL;
 		}
 
+		template<typename data_type>
+		struct LowBits
+		{
+			static data_type lowbits(unsigned int const b)
+			{
+				unsigned int const dbits = CHAR_BIT * sizeof(data_type);
+
+				if ( b < dbits )
+				{
+					return (static_cast<data_type>(1) << b) - static_cast<data_type>(1);
+				}
+				else
+				{
+					unsigned int const dbits_high = dbits/2;
+					unsigned int const dbits_low = dbits - dbits_high;
+
+					data_type const high = LowBits<data_type>::lowbits(dbits_high);
+					data_type const low  = LowBits<data_type>::lowbits(dbits_low);
+
+					return
+						(high << dbits_low) | low;
+				}
+			}
+		};
 	}
 }
 #endif
