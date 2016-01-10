d5272c957238c5d3c3e19502ec10573f6a907562
German Tischler <gt1@sanger.ac.uk>
2016-01-10 21:56:39 +0100

    add mergeWithEq function in GammaDifferenceMerge

--- src/libmaus2/gamma/GammaDifferenceMerge.hpp
+++ src/libmaus2/gamma/GammaDifferenceMerge.hpp
@@ -171,6 +171,52 @@ namespace libmaus2
 				}
 			}
 
+			static void mergeWithEq(std::ostream & out, std::istream & in0, std::istream & in1, int64_t const rprev = -1)
+			{
+				libmaus2::gamma::GammaDifferenceDecoder<data_type,mindif> GD0(in0);
+				libmaus2::gamma::GammaDifferenceDecoder<data_type,mindif> GD1(in1);
+				libmaus2::gamma::GammaDifferenceEncoder<data_type,mindif> GE(out,rprev);
+
+				data_type d0;
+				data_type d1;
+				bool haved0 = GD0.decode(d0);
+				bool haved1 = GD1.decode(d1);
+
+				while ( haved0 && haved1 )
+				{
+					if ( d0 < d1 )
+					{
+						GE.encode(d0);
+						haved0 = GD0.decode(d0);
+					}
+					else if ( d1 < d0 )
+					{
+						GE.encode(d1);
+						haved1 = GD1.decode(d1);
+					}
+					else
+					{
+						assert ( d0 == d1 );
+
+						GE.encode(d0);
+						haved0 = GD0.decode(d0);
+						haved1 = GD1.decode(d1);
+					}
+				}
+
+				while ( haved0 )
+				{
+					GE.encode(d0);
+					haved0 = GD0.decode(d0);
+				}
+
+				while ( haved1 )
+				{
+					GE.encode(d1);
+					haved1 = GD1.decode(d1);
+				}
+			}
+
 			static void filterOut(std::ostream & out, std::istream & in0, std::istream & in1, int64_t const rprev = -1)
 			{
 				libmaus2::gamma::GammaDifferenceDecoder<data_type,mindif> GD0(in0);
@@ -223,6 +269,14 @@ namespace libmaus2
 				libmaus2::aio::InputStreamInstance ISI1(in1);
 				libmaus2::aio::OutputStreamInstance OSI(out);
 				merge(OSI,ISI0,ISI1,prev);
+			}
+
+			static void mergeWithEq(std::string const & out, std::string const & in0, std::string const & in1, int64_t const prev = -1)
+			{
+				libmaus2::aio::InputStreamInstance ISI0(in0);
+				libmaus2::aio::InputStreamInstance ISI1(in1);
+				libmaus2::aio::OutputStreamInstance OSI(out);
+				mergeWithEq(OSI,ISI0,ISI1,prev);
 			}
 
 			static void filterOut(std::string const & out, std::string const & in0, std::string const & in1, int64_t const prev = -1)
