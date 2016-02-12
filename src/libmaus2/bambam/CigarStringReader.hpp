1083aaa747a4636ff6d5c54040cd0d02fcdc8f11
German Tischler <tischler@mpi-cbg.de>
2016-02-12 12:25:17 +0100

    add CigarStringReader class for reading block encoded cigar operations operation by operation

---
+++ src/libmaus2/bambam/CigarStringReader.hpp
@@ -0,0 +1,65 @@
+/*
+    libmaus2
+    Copyright (C) 2015 German Tischler
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#if ! defined(LIBMAUS2_BAMBAM_CIGARSTRINGREADER_HPP)
+#define LIBMAUS2_BAMBAM_CIGARSTRINGREADER_HPP
+
+#include <libmaus2/bambam/BamFlagBase.hpp>
+
+namespace libmaus2
+{
+	namespace bambam
+	{
+		template<typename iterator>
+		struct CigarStringReader
+		{
+			iterator it_c;
+			iterator it_e;
+			cigar_operation current;
+
+			CigarStringReader()
+			: it_c(iterator()), it_e(iterator()), current()
+			{
+
+			}
+
+			CigarStringReader(iterator rit_c, iterator rit_e)
+			: it_c(rit_c), it_e(rit_e), current(cigar_operation(0,0))
+			{
+
+			}
+
+			bool getNext(::libmaus2::bambam::BamFlagBase::bam_cigar_ops & op)
+			{
+				while ( (! current.second) && (it_c != it_e) )
+					current = *(it_c++);
+
+				if ( current.second )
+				{
+					current.second -= 1;
+					op = current.first;
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+			}
+		};
+	}
+}
+#endif
