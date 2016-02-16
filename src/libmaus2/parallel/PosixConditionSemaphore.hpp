f3d712222f9c07f87a475cac386424c277517414
German Tischler <tischler@mpi-cbg.de>
2016-02-16 09:56:04 +0100

    add pointer types in PosixConditionSemaphore

--- src/libmaus2/parallel/PosixConditionSemaphore.hpp
+++ src/libmaus2/parallel/PosixConditionSemaphore.hpp
@@ -33,6 +33,10 @@ namespace libmaus2
 	{
 		struct PosixConditionSemaphore : public SimpleSemaphoreInterface
 		{
+			typedef PosixConditionSemaphore this_type;
+			typedef libmaus2::util::unique_ptr<this_type>::type unique_ptr_type;
+			typedef libmaus2::util::shared_ptr<this_type>::type shared_ptr_type;
+
 			pthread_cond_t cond;
 			pthread_mutex_t mutex;
 			int volatile sigcnt;
