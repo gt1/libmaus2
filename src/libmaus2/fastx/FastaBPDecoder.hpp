19f489fe0bcd0632745387c49005c7c7a4bc4eab
German Tischler <tischler@mpi-cbg.de>
2016-02-24 17:42:02 +0100

    parameterise decodeSequencesParallel in FastaBPDecoder by memory allocation model

--- src/libmaus2/fastx/FastaBPDecoder.hpp
+++ src/libmaus2/fastx/FastaBPDecoder.hpp
@@ -330,9 +330,10 @@ namespace libmaus2
 				uint64_t length;
 			};
 
+			template<libmaus2::autoarray::alloc_type atype>
 			void decodeSequencesParallel(
 				std::string const & fn, uint64_t const numthreads,
-				libmaus2::autoarray::AutoArray<char> & Aseq,
+				libmaus2::autoarray::AutoArray<char,atype> & Aseq,
 				std::vector<SequenceMeta> & Vseqmeta,
 				bool const pad = false,
 				char padsym = 4
@@ -376,7 +377,7 @@ namespace libmaus2
 				sum += (pad ? 1 : 0);
 
 				Aseq.release();
-				Aseq = libmaus2::autoarray::AutoArray<char>(sum,false);
+				Aseq = libmaus2::autoarray::AutoArray<char,atype>(sum,false);
 
 				#if defined(_OPENMP)
 				#pragma omp parallel for num_threads(numthreads) schedule(dynamic,1)
