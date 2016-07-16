e7481baca317dac98f35939f062c6f31d9807fd3
German Tischler <tischler@mpi-cbg.de>
2016-07-16 18:43:06 +0200

    add DNARankSuffixTreeNodeEnumerator class

--- src/test/testdnarank.cpp
+++ src/test/testdnarank.cpp
@@ -15,6 +15,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+#include <libmaus2/rank/DNARankSuffixTreeNodeEnumerator.hpp>
 #include <libmaus2/rank/DNARank.hpp>
 #include <libmaus2/rank/DNARankBiDir.hpp>
 #include <libmaus2/util/ArgParser.hpp>
