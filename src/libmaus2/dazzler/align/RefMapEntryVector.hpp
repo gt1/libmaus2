8f72faa2b37acbc42c8a638b13d7f7dea8952e27
German Tischler <tischler@mpi-cbg.de>
2016-08-02 12:10:51 +0200

    add support for trimmed databases in RefMapEntryVector

--- src/libmaus2/dazzler/align/RefMapEntryVector.hpp
+++ src/libmaus2/dazzler/align/RefMapEntryVector.hpp
@@ -83,7 +83,7 @@ namespace libmaus2
 						libmaus2::fastx::FastAReader::pattern_type pattern;
 						RefMapEntryVector V;
 
-						// uint64_t outid = 0;
+						uint64_t outid = 0;
 						for ( uint64_t inid = 0; FA.getNextPatternUnlocked(pattern); ++inid )
 						{
 							std::string const spat = pattern.spattern;
@@ -108,12 +108,81 @@ namespace libmaus2
 									)
 										++h;
 
+									assert ( outid == V.size() );
+
 									RefMapEntry E(inid,l);
 									V.push_back(E);
 
 									assert ( h > l );
 
 									l = h;
+									outid += 1;
+								}
+							}
+						}
+
+						V.serialise(refmapfn);
+
+						return V;
+					}
+					else
+					{
+						return RefMapEntryVector(refmapfn);
+					}
+
+				}
+
+				template<typename db_type>
+				static RefMapEntryVector computeRefSplitMap(std::string const reffn, db_type const & DB)
+				{
+					std::string const refmapfn = reffn + ".refmap";
+
+					if (
+						(! libmaus2::util::GetFileSize::fileExists(refmapfn))
+						||
+						(libmaus2::util::GetFileSize::isOlder(refmapfn,reffn))
+					)
+					{
+						libmaus2::fastx::FastAReader FA(reffn);
+						libmaus2::fastx::FastAReader::pattern_type pattern;
+						RefMapEntryVector V;
+
+						// id in untrimmed database
+						uint64_t outid = 0;
+						for ( uint64_t inid = 0; FA.getNextPatternUnlocked(pattern); ++inid )
+						{
+							std::string const spat = pattern.spattern;
+							uint64_t l = 0;
+
+							while ( l < spat.size() )
+							{
+								uint64_t h = l;
+								while (
+									h < spat.size() &&
+									libmaus2::fastx::mapChar(spat[h]) >= 4
+								)
+									++h;
+
+								if ( h < spat.size() )
+								{
+									l = h;
+
+									while (
+										h < spat.size() &&
+										libmaus2::fastx::mapChar(spat[h]) < 4
+									)
+										++h;
+
+									if ( DB.isInTrimmed(outid) )
+									{
+										RefMapEntry E(inid,l);
+										V.push_back(E);
+									}
+
+									assert ( h > l );
+
+									l = h;
+									outid += 1;
 								}
 							}
 						}
