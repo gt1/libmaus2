bbc4629286903f66e8f5de676a3ff8683fec0e13
German Tischler <gt1@sanger.ac.uk>
2016-08-23 22:21:12 +0200

    fix typedef

--- src/libmaus2/suffixsort/bwtb3m/BwtMergeSortResult.hpp
+++ src/libmaus2/suffixsort/bwtb3m/BwtMergeSortResult.hpp
@@ -163,7 +163,7 @@ namespace libmaus2
 
 				struct CompactBareSimpleSampledSuffixArray
 				{
-					typedef CompactBareSimpleSampledSuffixArray;
+					typedef CompactBareSimpleSampledSuffixArray this_type;
 					typedef libmaus2::util::unique_ptr<this_type>::type unique_ptr_type;
 					typedef libmaus2::util::shared_ptr<this_type>::type shared_ptr_type;
 
