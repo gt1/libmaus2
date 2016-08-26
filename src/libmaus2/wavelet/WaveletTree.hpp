7fa8887381e30cfadc324f3ec961f8e9ad3cc1ea
German Tischler <tischler@mpi-cbg.de>
2016-08-26 14:37:18 +0200

    fix unitialised field bug in WaveletTree class

--- src/libmaus2/wavelet/WaveletTree.hpp
+++ src/libmaus2/wavelet/WaveletTree.hpp
@@ -1612,8 +1612,8 @@ namespace libmaus2
 				symbol_type sym;
 
 				EnumerateRangeSymbolsStackElement() : NodePortion(), visit(0), sym(0) {}
-				EnumerateRangeSymbolsStackElement(NodePortion const & portion) : NodePortion(portion), sym(0) {}
-				EnumerateRangeSymbolsStackElement(NodePortion const & portion, symbol_type const rsym) : NodePortion(portion), sym(rsym) {}
+				EnumerateRangeSymbolsStackElement(NodePortion const & portion) : NodePortion(portion), sym(0), visit(0) {}
+				EnumerateRangeSymbolsStackElement(NodePortion const & portion, symbol_type const rsym) : NodePortion(portion), sym(rsym), visit(0) {}
 
 				unsigned int depth() const
 				{
