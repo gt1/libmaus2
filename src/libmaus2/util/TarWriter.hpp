3f2f87ce1bdfeb89bc703ac4b28e6535e0483fb3
German Tischler <tischler@mpi-cbg.de>
2017-10-04 11:18:27 +0200

    add pointer types in TarWriter

--- src/libmaus2/util/TarWriter.hpp
+++ src/libmaus2/util/TarWriter.hpp
@@ -40,6 +40,10 @@ namespace libmaus2
 	{
 		struct TarWriter
 		{
+			typedef TarWriter this_type;
+			typedef libmaus2::util::unique_ptr<this_type>::type unique_ptr_type;
+			typedef libmaus2::util::shared_ptr<this_type>::type shared_ptr_type;
+		
 			#if defined(LIBMAUS2_HAVE_LIBARCHIVE)
 			uid_t const uid;
 			gid_t const gid;
