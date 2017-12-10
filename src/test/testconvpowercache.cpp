a0cf815069e21c2b7473eb2a92032816b67e2de5
tischler <tischler@mpi-cbg.de>
2017-12-10 20:42:43 +0100

    add test program for convolution cache classes

---
+++ src/test/testconvpowercache.cpp
@@ -0,0 +1,151 @@
+/*
+    libmaus2
+    Copyright (C) 2017 German Tischler
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <libmaus2/math/Convolution.hpp>
+
+static std::vector < double > getInitialPI(double const p_i)
+{
+	double const q_i = 1.0 - p_i;
+	double f_i = q_i;
+
+	std::vector < double > P_I;
+	while ( f_i >= 1e-7 )
+	{
+		P_I.push_back(f_i);
+		f_i *= p_i;
+	}
+
+	return P_I;
+}
+
+static std::vector < double > getInitialPD(double const p_d)
+{
+	std::vector < double > P_D;
+	P_D.push_back(1.0 - p_d);
+	P_D.push_back(p_d);
+	return P_D;
+}
+
+bool compareVectors(
+	std::vector<double> const & A,
+	std::vector<double> const & B,
+	double const eps
+)
+{
+	for ( uint64_t i = 0; i < std::min(A.size(),B.size()); ++i )
+		if ( std::abs(A[i]-B[i]) > eps )
+			return false;
+
+	for ( uint64_t i = A.size(); i < B.size(); ++i )
+		if ( std::abs(B[i]) > eps )
+			return false;
+
+	for ( uint64_t i = B.size(); i < A.size(); ++i )
+		if ( std::abs(A[i]) > eps )
+			return false;
+
+	return true;
+}
+
+bool testPowerCache(std::vector<double> const & V, unsigned int const l, double const eps)
+{
+	std::vector<double> C(1,1.0);
+
+	libmaus2::math::Convolution::PowerCache PC(V);
+
+	bool ok = true;
+	uint64_t const n = 1ull < l;
+	for ( uint64_t i = 0; ok && i < n; ++i )
+	{
+		ok = ok && compareVectors(C,PC[i],eps);
+	}
+
+	return ok;
+}
+
+bool testPowerCacheRussian(std::vector<double> const & V, unsigned int const l, unsigned int const maxshift, double const eps)
+{
+	std::vector<double> C(1,1.0);
+
+	bool ok = true;
+	for ( unsigned int shift = 0; shift < maxshift; ++shift )
+	{
+		libmaus2::math::Convolution::PowerCacheRussian PCR(V,l,shift*l,1 /* numthreads */);
+		libmaus2::math::Convolution::PowerCache PC(V);
+
+		uint64_t const n = 1ull << l;
+		uint64_t const subshift = shift * l;
+
+		std::cerr << "[V] testPowerCacheRussian shift=" << shift << " l=" << l << " n=" << n << std::endl;
+
+		for ( uint64_t i = 0; ok && i < n; ++i )
+		{
+			ok = ok && compareVectors(PC[i << subshift],PCR[i],eps);
+			std::cerr << "[V] testPowerCacheRussian shift=" << shift << " " << i << "/" << n << " ok=" << ok << std::endl;
+		}
+	}
+
+	return ok;
+}
+
+bool testPowerBlockCache(std::vector<double> const & V, unsigned int const l, double const eps)
+{
+	std::vector<double> C(1,1.0);
+
+	libmaus2::math::Convolution::PowerCache PC(V);
+	libmaus2::math::Convolution::PowerBlockCache PBC(V,3/*blocksize*/,1/*numthreads*/);
+
+	bool ok = true;
+	uint64_t const n = 1ull << (4*l);
+	for ( uint64_t i = 0; ok && i < n; ++i )
+		ok = ok && compareVectors(PC[i],PBC[i],eps);
+
+	return ok;
+}
+
+int main(int argc, char * argv[])
+{
+	try
+	{
+		std::vector<double> const V = getInitialPI(0.1);
+
+		bool const pcok = testPowerCache(V,8,1e-6);
+
+		std::cerr << "[V] testPowerCache " << (pcok?"ok":"failed") << std::endl;
+
+		bool const pcokrussian = testPowerCacheRussian(V,3,4,1e-6);
+
+		std::cerr << "[V] testPowerCacheRussian " << (pcokrussian?"ok":"failed") << std::endl;
+
+		bool const pcokblock = testPowerBlockCache(V,3,1e-6);
+
+		std::cerr << "[V] testPowerBlockCache " << (pcokblock?"ok":"failed") << std::endl;
+
+		bool const allok = pcok && pcokrussian;
+
+		if ( allok )
+			return EXIT_SUCCESS;
+		else
+			return EXIT_FAILURE;
+	}
+	catch(std::exception const & ex)
+	{
+		std::cerr << ex.what() << std::endl;
+		return EXIT_FAILURE;
+	}
+}
