587f412cd81110cee117abcc83058f915d2271e6
German Tischler <tischler@mpi-cbg.de>
2017-12-14 09:28:56 +0100

    create a single point where we include pthread.h

--- src/libmaus2/parallel/PosixThread.hpp
+++ src/libmaus2/parallel/PosixThread.hpp
@@ -16,9 +16,8 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-
-#if ! defined(POSIXMUTEXARRAY_HPP)
-#define POSIXMUTEXARRAY_HPP
+#if ! defined(POSIXTHREAD_HPP)
+#define POSIXTHREAD_HPP
 
 #include <libmaus2/autoarray/AutoArray.hpp>
 #include <libmaus2/parallel/PosixMutex.hpp>
