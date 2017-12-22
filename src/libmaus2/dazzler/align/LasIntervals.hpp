23404e03cc7e25c94ffc24872901ff0e64c57fec
German Tischler <tischler@mpi-cbg.de>
2017-12-22 12:54:43 +0100

    whitespace cleanup

--- src/libmaus2/dazzler/align/LasIntervals.hpp
+++ src/libmaus2/dazzler/align/LasIntervals.hpp
@@ -32,7 +32,7 @@ namespace libmaus2
 				typedef LasIntervals this_type;
 				typedef libmaus2::util::unique_ptr<this_type>::type unique_ptr_type;
 				typedef libmaus2::util::shared_ptr<this_type>::type shared_ptr_type;
-			
+
 				struct LasIntervalsIndexEntry
 				{
 					uint64_t from;
