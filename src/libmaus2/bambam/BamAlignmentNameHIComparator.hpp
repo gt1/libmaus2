3dd4effd21ff0709e0aff41b4eae65c9b1211221
German Tischler <tischler@mpi-cbg.de>
2018-01-05 11:15:49 +0100

    fix include guard in BamAlignmentNameHIComparator.hpp

--- src/libmaus2/bambam/BamAlignmentNameHIComparator.hpp
+++ src/libmaus2/bambam/BamAlignmentNameHIComparator.hpp
@@ -16,8 +16,8 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-#if ! defined(LIBMAUS2_BAMBAM_BAMALIGNMENTNAMECOMPARATOR_HPP)
-#define LIBMAUS2_BAMBAM_BAMALIGNMENTNAMECOMPARATOR_HPP
+#if ! defined(LIBMAUS2_BAMBAM_BAMALIGNMENTNAMEHICOMPARATOR_HPP)
+#define LIBMAUS2_BAMBAM_BAMALIGNMENTNAMEHICOMPARATOR_HPP
 
 #include <libmaus2/bambam/BamAlignment.hpp>
 #include <libmaus2/bambam/StrCmpNum.hpp>
