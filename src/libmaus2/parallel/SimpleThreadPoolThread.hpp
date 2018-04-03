7d67d7b696e584aee5538aca87c1056b01539bdd
German Tischler <tischler@mpi-cbg.de>
2018-04-03 17:28:52 +0200

    fix include guards in SimpleThreadPoolThread.hpp

--- src/libmaus2/parallel/SimpleThreadPoolThread.hpp
+++ src/libmaus2/parallel/SimpleThreadPoolThread.hpp
@@ -16,8 +16,8 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-#if ! defined(LIBMAUS2_PARALLEL_THREADPOOLTHREAD_HPP)
-#define LIBMAUS2_PARALLEL_THREADPOOLTHREAD_HPP
+#if ! defined(LIBMAUS2_PARALLEL_SIMPLETHREADPOOLTHREAD_HPP)
+#define LIBMAUS2_PARALLEL_SIMPLETHREADPOOLTHREAD_HPP
 
 #include <libmaus2/parallel/PosixThread.hpp>
 #include <libmaus2/parallel/SimpleThreadPoolInterface.hpp>
